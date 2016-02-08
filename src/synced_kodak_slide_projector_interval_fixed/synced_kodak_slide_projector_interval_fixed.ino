e3811c687c1d7afc71ff59539d6b63cb5aac30da
Mitsuhito Ando <mitsuhito.ando@gmail.com>
2016-02-08 18:03:02 +0900

    add synced_kodak_slide_projector_interval_fixed.ino

---
+++ src/synced_kodak_slide_projector_interval_fixed/synced_kodak_slide_projector_interval_fixed.ino
@@ -0,0 +1,134 @@
+/*
+   Copyright 2013 Mitsuhito Ando
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// 設定するスライド時間間隔(15000ミリ秒)
+const int DELAY_MS = 15000;
+
+// 再生中かどうかのフラグ
+volatile boolean is_playing = false;
+
+// ボタン1の状態を表す変数
+volatile boolean button_1_input_status = LOW;
+
+// ボタン2の状態を表す変数
+volatile boolean button_2_input_status = LOW;
+
+// 最後にボタン1が押された時の時間を保持する変数
+volatile unsigned long button_1_pressed_time = 0;
+
+// 最後にボタン2が押された時の時間を保持する変数
+volatile unsigned long button_2_pressed_time = 0;
+
+// チャタリングを無視する時間定数(ミリ秒)
+const int BOUNCE = 100;
+
+// 入出力ピンの番号設定
+const int BUTTON_1_PIN = 2;
+const int BUTTON_2_PIN = 3;
+const int RELAY_1_PIN = 4;
+const int RELAY_2_PIN = 5;
+const int BUTTON_1_STATUS_LED_PIN = 12;
+const int BUTTON_2_STATUS_LED_PIN = 13;
+const int POTENTIOMETER_PIN = A0;
+
+//
+// ピンモードの設定、割り込みピンの設定、シリアルポートの初期化
+//
+void setup()
+{
+  pinMode(BUTTON_1_STATUS_LED_PIN, OUTPUT);
+  pinMode(BUTTON_2_STATUS_LED_PIN, OUTPUT);
+
+  pinMode(RELAY_1_PIN, OUTPUT);
+  pinMode(RELAY_2_PIN, OUTPUT);
+
+  pinMode(POTENTIOMETER_PIN, INPUT);
+
+  pinMode(BUTTON_1_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_2_PIN, INPUT_PULLUP);
+
+  attachInterrupt(0, button_1_pressed, FALLING);
+  attachInterrupt(1, button_2_pressed, FALLING);
+
+  Serial.begin(9600);
+}
+
+//
+// ループ関数
+//
+void loop()
+{
+
+  // デバッグ用
+  Serial.print("DELAY_MS=");
+  Serial.print(DELAY_MS);
+  Serial.print(" is_playing=");
+  Serial.println(is_playing);
+
+  // 再生ボタンが押されていた場合
+  if(is_playing)
+  {
+    // スライドプロジェクタAを先にスライドさせる信号を送る
+    PORTD |= B00010000;
+    delay(20);
+
+    // スライドプロジェクタBをスライドさせる信号を送る
+    PORTD |= B00100000;
+
+    // AとB同時にスライドさせる信号を送る
+    //PORTD |= B00110000;
+    
+    // リレー信号を200ms保持する
+    delay(200);
+    
+    // AとBのスライドプロジェクタのスライドを止める
+    PORTD &= B11001111;
+    
+    // 可変抵抗で読み取った時間間隔分停止を保持する
+    delay(DELAY_MS);
+  }
+}
+
+//
+// ボタン1が押された時の処理
+//
+void button_1_pressed()
+{
+  // 再生する
+  is_playing = true;
+  
+  if((millis() - button_1_pressed_time) > BOUNCE)
+    button_1_input_status = !button_1_input_status;
+  
+  button_1_pressed_time = millis();
+  digitalWrite(BUTTON_1_STATUS_LED_PIN, button_1_input_status);
+}
+
+//
+// ボタン2が押された時の処理
+//
+void button_2_pressed()
+{
+  // 停止する
+  is_playing = false;
+
+  if((millis() - button_2_pressed_time) > BOUNCE)
+    button_2_input_status = !button_2_input_status;
+
+  button_2_pressed_time = millis();
+  digitalWrite(BUTTON_2_STATUS_LED_PIN, button_2_input_status);
+}
+
